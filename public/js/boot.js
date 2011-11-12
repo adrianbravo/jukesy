57ba9f3025d7ba02062ad7ec32ef1a94015625ff
Adrian Bravo <me@adrianbravo.net>
2011-11-11 23:59:56 -0800

    Hiding body until js/css is loaded.

--- public/js/boot.js
+++ public/js/boot.js
@@ -3,6 +3,8 @@ window.Collection = {};
 window.Model = {};
 window.View = {};
 
+$('#app').css('opacity', '1.0');
+
 // YouTube API Player callback
 // The function name cannot be changed.
 function onYouTubePlayerReady(id) {
