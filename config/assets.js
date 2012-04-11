5e423a77deed204a8e41bda877e1d2deb26d4bff
Adrian Bravo <me@adrianbravo.net>
2012-04-11 04:36:41 -0700

    assets no longer begin with / by default

--- config/assets.js
+++ config/assets.js
@@ -1,37 +1,37 @@
 var assets = {
 
   js: [
-    'js/lib/json2.js',
-    'js/lib/jquery-1.7.2.min.js',
-    'js/lib/underscore-min.js',
-    'js/lib/backbone.js',
-    'js/lib/swfobject.js',
-    'js/lib/moment-1.5.0.js',
-    'js/lib/less-1.3.0.min.js',
-    'js/lib/jade.min.js',
-    'js/lib/bootstrap.js',
-    'js/boot.js',
-    'js/mixins.js',
-    'js/error.js',
-    'js/modal.js',
+    '/js/lib/json2.js',
+    '/js/lib/jquery-1.7.2.min.js',
+    '/js/lib/underscore-min.js',
+    '/js/lib/backbone.js',
+    '/js/lib/swfobject.js',
+    '/js/lib/moment-1.5.0.js',
+    '/js/lib/less-1.3.0.min.js',
+    '/js/lib/jade.min.js',
+    '/js/lib/bootstrap.js',
+    '/js/boot.js',
+    '/js/mixins.js',
+    '/js/error.js',
+    '/js/modal.js',
 
-    'js/user.js',
-    'js/session.js',
-    'js/lastfm.js',
-    'js/search.js',
-    'js/playlist.js',
-    'js/artist.js',
-    'js/album.js',
-    'js/track.js',
-    'js/tag.js',
-    'js/meow.js',
+    '/js/user.js',
+    '/js/session.js',
+    '/js/lastfm.js',
+    '/js/search.js',
+    '/js/playlist.js',
+    '/js/artist.js',
+    '/js/album.js',
+    '/js/track.js',
+    '/js/tag.js',
+    '/js/meow.js',
 
-    'js/video.js',
-    'js/keys.js',
-    'js/application.js'
+    '/js/video.js',
+    '/js/keys.js',
+    '/js/application.js'
   ],
   less: [
-    'less/bootstrap.less'
+    '/less/bootstrap.less'
   ],
   css: []
 
@@ -42,7 +42,7 @@ exports.staging = assets
 exports.test = assets
 
 exports.production = {
-  js: [ 'jukesy.min.js' ],
-  css: [ 'jukesy.css' ],
+  js: [ '/jukesy.min.js' ],
+  css: [ '/jukesy.css' ],
   less: []
 }
